f58d48eaee0d589814a4cae47459a65f409c0425
aifrak <>
2021-04-15 10:12:55 +0200

    Import up-to-date utils files

--- scripts/makefile/makefile-utils/docker-hub.mk
+++ scripts/makefile/makefile-utils/docker-hub.mk
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Source: https://github.com/aifrak/makefile-utils
-
 include $(MAKEFILE_UTILS_DIR)/makefile-utils.mk
 
 SHELL := sh
@@ -13,10 +11,12 @@ DOCKER_COMPOSE := docker-compose
 .PHONY: docker-hub-sut
 docker-hub-sut:
 	@echo ""
-	@echo "- Test Docker image build (Docker Hub sut):"
+	@echo "Test Docker image build (Docker Hub sut):"
 	@echo ""
 	@$(DOCKER_COMPOSE) --file docker-compose.test.yml build \
 		--progress tty --quiet base-image sut && \
 	$(DOCKER_COMPOSE) --file docker-compose.test.yml up sut && \
 	$(DOCKER_COMPOSE) --file docker-compose.test.yml down \
 		--volumes --remove-orphans --rmi all
+	@echo ""
+	@echo $(call print_success, "✔ Test Docker image build (Docker Hub sut): OK")
--- scripts/makefile/makefile-utils/makefile-utils.mk
+++ scripts/makefile/makefile-utils/makefile-utils.mk
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# Source: https://github.com/aifrak/makefile-utils
-
 # Colors
 GREEN = $(shell echo "\e[32m")
 NORMAL = $(shell echo "\e[0m")
 
 # Print a success message
 # Input:	$(1) -> string Success message to print
-print_success = "$(GREEN)✔$(1)$(NORMAL)"
+print_success = "$(GREEN)$(1)$(NORMAL)"
